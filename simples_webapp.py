8a818b265c7dbb946218b73a3fb5761e6b31094f
Victorhxm <victorxaviermarques@hotmail.com>
2025-02-27 21:31:38 -0300

    Adicionado checker

--- simples_webapp.py
+++ simples_webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, session
+from checker import check_logged_in
 
 app = Flask(__name__)
 
@@ -7,14 +8,17 @@ def hello() -> str:
     return 'Hello from the simples webapp.'
 
 @app.route('/page1')
+@check_logged_in
 def page1() -> str:
     return 'This is page 1.'
 
 @app.route('/page2')
+@check_logged_in
 def page2() -> str:
     return 'This is page 2.'
 
 @app.route('/page3')
+@check_logged_in
 def page3() -> str:
     return 'This is page 3.'
 
@@ -23,19 +27,12 @@ def do_login ()-> str:
     session['logged_in'] = True
     return 'You are now logged in.'
 
-app.secret_key = 'computador'
-
-@app.route('/status')
-def check_status() -> str:
-    if 'loggedin_in'in session:
-        return'you are currently logged in'
-
-    return'You are NOT logged in'
-
 @app.route('/logout')
 def do_logout() -> str:
     session.pop('logged_in')
     return 'You are now looged out'
+
+app.secret_key = 'computador'
 
 if __name__ ==  '__main__':
     app.run(debug=True)
